1cf525e2ba2666ca76160d069aa72ecf810fbba1
ragnorak1000 <ragnorak1000@gmail.com>
2014-12-20 19:13:21 -0500

    roll a ball project
    rolll a ball project

---
+++ roll-a-ball/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour 
+{
+
+	public float speed;
+
+	void FixedUpdate () 
+	{
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+
+		Vector3 vector = new Vector3 (moveHorizontal, 0f, moveVertical);
+
+		rigidbody.AddForce (vector * speed * Time.deltaTime);	
+	}
+}
