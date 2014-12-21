033b70afa6bbae3c6bc2367c10e19452f2c45d01
ragnorak1000 <ragnorak1000@gmail.com>
2014-12-20 20:34:01 -0500

    adding pick ups

---
+++ roll-a-ball/Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject playerObject;
+	private Vector3 offset;
+
+	void Start () {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = playerObject.transform.position + offset;
+	}
+}
---
+++ roll-a-ball/Assets/Scripts/PickupRotator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PickupRotator : MonoBehaviour {
+
+	public Vector3 rotator; 
+
+	void Update () {
+		transform.Rotate (rotator * Time.deltaTime);
+	}
+}
